1507d418ba9203f458c9df44c28b607767e2ebad
Winters <luke.s.winters@gmail.com>
2014-11-21 17:45:17 -0800

    google updates

---
+++ src/Google/DiagonalLineSum.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Google;
+
+/**
+ * 
+ * 题目是给一个n*m的二维数组返回一个一维数组，这个数组包含之前数组的对角线的和。下面是例子：
+ * [1, 2, 3.
+ * 4, 5, 6
+ * 7, 8, 9] 要返回 [7, 12, 15, 8, 3]
+ * @author winters
+ *
+ */
+public class DiagonalLineSum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Google/HandleWordinString.java
@@ -0,0 +1,19 @@
+package Google;
+
+/**
+ * 
+ * 题目是给一个string，返回含有word的list。word的定义是空格(大于等于一个）之间的或者引号之间的，
+ * 如果引号里面有空格要做为一个word返回。
+ * 比如string是 I    have a "faux coat" 要返回[I, have, a, faux coat]
+ * 
+ * @author winters
+ *
+ */
+public class HandleWordinString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
